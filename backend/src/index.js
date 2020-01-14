2717c18a9909415badebc2466024a1e4c13bd900
Kássia Catarine <kassia_catarine.15@hotmail.com>
2020-01-14 17:40:42 -0300

    Criando o router de devs e a rota de criação de um dev

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const devsRouter = require('./routes/devs.router');
 
 const app = express();
 
-mongoose.connect(process.env.DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).catch((reason) => {
+    console.log('Unable to connect to the mongodb instance. Error: ', reason);
+  });
 
 app.use(express.json());
+
+app.use('/devs', devsRouter);
 
 app.listen(3000);
