37c5ba131ba35832a44398c95ccedeb2f919e8d4
Kássia Catarine <kassia_catarine.15@hotmail.com>
2020-01-14 17:44:24 -0300

    Criando controller de dev para separar logica

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const Dev = require('../models/Dev.model');
+
+module.exports = {
+  async store(req, res) {
+    const {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    } = req.body;
+    const githubResponse = axios.get(`https://api.github.com/users/${github_username}`);
+
+    const {
+      name = login, avatar_url, bio
+    } = githubResponse.data;
+
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const location = {
+      type: 'Point',
+      coordinates: [longitude, latitude]
+    }
+
+    const dev = await Dev.create({
+      github_username,
+      name,
+      avatar_url,
+      bio,
+      techs: techsArray,
+      location
+    })
+
+    return res.json(dev);
+  }
+}
